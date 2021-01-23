cf847b628d4ec6b0cbef53cfab8b87d62ac3a20e
João Eurico "Bokomoko" Lima <34620239+Bokomoko@users.noreply.github.com>
2021-01-23 00:35:27 -0300

    fixed some bugs, added login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,28 @@
-import Head from 'next/head'
+import { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import { signIn, signOut, useSession } from 'next-auth/client'
+import Nav from "../components/nav"
+export default function Home(): NextPage {
+  const [session, loading] = useSession()
 
-export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>BBcita (a Nani Milet Company</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <img src="./bybbcita.png" alt="bbcitalogo"></img>
+    <div>
+      <Nav>
+        {!session && (<div className="text-3xl">
+          Not signed in <br />
+          <button onClick={(): Promise<void> => signIn()}>Sign in</button>
+        </div>)}
+        {session && (<div className="text-3xl">
+          Signed in as {session.user.email} <br />
+          <button onClick={(): Promise<void> => signOut()}>Sign out</button>
+        </div>)}
+      </Nav>
+      < div className={styles.container}>
 
-          <h1>Página em construção</h1>
-          <h2>Em breve, bbcita store</h2>
-    </div>
+        <img src="./bybbcita.png" alt="bbcitalogo"></img>
+        <h1>Página em construção</h1>
+        <h2>Em breve, bbcita store</h2>
+      </div>
+    </div >
   )
 }
